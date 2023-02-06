ec0c61ba7bde381bcef097cc9f9ca2acfa8b9e1b
amr8644 <amr8644@gmail.com>
2023-02-06 18:20:39 +0300

    Sending Whatsapp messages using twillo

---
+++ go.mod
@@ -0,0 +1,10 @@
+module api/message
+
+go 1.19
+
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/twilio/twilio-go v1.3.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	twilio "github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+func main() {
+	
+	//  Load .env variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}	
+
+	// Store the .env values in a variable
+	var ACCOUND_SID string = os.Getenv("ACCOUND_SID")
+	var ACCOUND_KEY string = os.Getenv("ACCOUND_KEY")
+	var FROM string = os.Getenv("FORM")
+
+	// Initialize twilio client
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username:  ACCOUND_SID,
+		Password: ACCOUND_KEY,
+	})
+
+	// Put all the number in an array
+	sent_to := []string{"number","num","num","num"} 
+
+
+	// Loop throuh the array
+	for _,num := range sent_to{
+		params := &openapi.CreateMessageParams{}
+		// Set the numbers
+		params.SetTo("whatsapp:" + num)
+		params.SetFrom("whatsapp:" + FROM)
+		// Set the body
+		params.SetBody("Hello World")
+		_, err := client.Api.CreateMessage(params)
+		// Catch error
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			fmt.Println("Message sent successfully!")
+		}
+	}
+}
