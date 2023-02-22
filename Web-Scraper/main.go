c1cebdd2164b7e39e055fcbe1c29801cf7b7117e
amr8644 <amr8644@gmail.com>
2023-02-22 21:04:36 +0300

    Fixed some bugs

--- Web-Scraper/main.go
+++ Web-Scraper/main.go
@@ -4,50 +4,79 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-// var m map[string]string
+var m1 map[string]string
 
 func ExampleScrape() {
-  // Request the HTML page.
-  res, err := http.Get("https://www.the-numbers.com/box-office-records/worldwide/all-movies/cumulative/all-time")
-  if err != nil {
-    log.Fatal(err)
-  }
+	// Request the HTML page.
+	res, err := http.Get("https://www.the-numbers.com/box-office-records/worldwide/all-movies/cumulative/all-time")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  defer res.Body.Close()
-  if res.StatusCode != 200 {
-    log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
-  }
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+	}
 
-  // Load the HTML document
-  doc, err := goquery.NewDocumentFromReader(res.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
-  
-  // Find the review items
-//   m1 := make(map[string]string)  
-  doc.Find("table").Each(func(i int, s *goquery.Selection) {
-	// var h,d string
-	s.Find("thead").Each(func(i int, head *goquery.Selection) {
-		  fmt.Println(head.Find("th").Text())
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	m1 := make(map[string]string)
+	doc.Find("table").Each(func(i int, table *goquery.Selection) {
+
+		table.Find("tbody").Each(func(i int, body *goquery.Selection) {
+			body.Find("tr").Each(func(i int, rowbody *goquery.Selection) {
+				rowbody.Find("td").Each(func(i int, titlebody *goquery.Selection) {
+					table.Find("thead").Each(func(i int, head *goquery.Selection) {
+						head.Find("tr").Each(func(i int, rowhead *goquery.Selection) {
+							rowhead.Find("th").Each(func(i int, titlehead *goquery.Selection) {
+								fmt.Println(titlehead.Text(), titlebody.Text())
+							})
+						})
+					})
+				})
+
+			})
+		})
 
 	})
-})
 
-
+	for key, ele := range m1 {
+		fmt.Println(key, ele)
+	}
 }
 
 func main() {
-  ExampleScrape()
+	start := time.Now()
+	ExampleScrape()
+	elapsed := time.Since(start)
+	fmt.Printf("\n\nTime took %s", elapsed)
 }
 
-// s.Find("tbody").Each(func(i int, de *goquery.Selection) {
-	// 	// fmt.Println(de.Find("tr").Text())
-	// 	d = de.Find("tr").Text()
-	// })
+// jsonStr, err := json.Marshal(m1)
+// if err != nil {
+// 	fmt.Printf("Error: %s", err.Error())
+// } else {
+// 	fmt.Println(string(jsonStr))
+// }
 
-	// m1[h] = d
+// file, _ := json.MarshalIndent(string(jsonStr), "", " ")
+
+// _ = ioutil.WriteFile("test.json", file, 0644)
+
+// table.Find("thead").Each(func(i int, head *goquery.Selection) {
+// 	head.Find("tr").Each(func(i int, rowhead *goquery.Selection) {
+// 		rowhead.Find("th").Each(func(i int, s *goquery.Selection) {
+// 			m1[s.Text()] = b.Text()
+// 		})
+// 	})
+// })
