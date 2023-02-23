2ae9f43d59a29431487ebf3d282ed224fbbb67fc
amr8644 <amr8644@gmail.com>
2023-02-23 23:19:40 +0300

    Fixed some bugs...

--- Table-Scraper/main.go
+++ Table-Scraper/main.go
@@ -12,14 +12,11 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-// A Writer wraps a csv.Writer to write records as maps of column names to
-// values, instead of lists of values.
 type Writer struct {
 	w      *csv.Writer
 	header []string
 }
 
-// NewWriter returns a new Writer that writes to w.
 func NewWriter(w *csv.Writer, header []string) *Writer {
 	return &Writer{
 		w:      w,
@@ -27,14 +24,10 @@ func NewWriter(w *csv.Writer, header []string) *Writer {
 	}
 }
 
-// WriteHeader writes the CSV header to w along with any necessary quoting.
 func (w *Writer) WriteHeader() error {
 	return w.w.Write(w.header)
 }
 
-// Writer writes a single CSV record to w along with any necessary quoting. A
-// record is a map of column names to values. Only columns present in the
-// Writer's header are written, and in the order they appear in the header.
 func (w *Writer) Write(record map[string]string) error {
 	s := make([]string, len(w.header))
 	for i, name := range w.header {
@@ -43,7 +36,6 @@ func (w *Writer) Write(record map[string]string) error {
 	return w.w.Write(s)
 }
 
-// WriteAll writes multiple CSV records to w using Write and then calls w.Flush.
 func (w *Writer) WriteAll(records []map[string]string) error {
 	for _, record := range records {
 		err := w.Write(record)
@@ -55,44 +47,59 @@ func (w *Writer) WriteAll(records []map[string]string) error {
 	return w.w.Error()
 }
 
-func WriterToCSV(data []map[string]string, header []string) {
+func RemoveDuplicates(header []string) []string {
+	check := make(map[string]int)
+	res := make([]string, 0)
+	for _, val := range header {
+		check[val] = 1
+	}
+
+	for letter, _ := range check {
+		res = append(res, letter)
+	}
+
+	return res
+}
+
+func WriteToCSV(data []map[string]string, header []string) {
 	f, err := os.OpenFile("data.csv", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		log.Fatalf("failed creating file: %s", err)
 	}
 
-	w := NewWriter(csv.NewWriter(f), header)
+	unique_headers := RemoveDuplicates(header)
+	w := NewWriter(csv.NewWriter(f), unique_headers)
 
 	err = w.WriteHeader()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println(header)
-	// for i := 0; i < len(data); i++ {
-		// err := w.Write(data[i])
-		for _, v := range data {
-			fmt.Println(v)
-		}
-		
-	// }
+
+	err = w.WriteAll(data)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	f.Close()
 }
 
 func WriteToJSON(data []map[string]string) {
 	// Write inside the file
-	byte, err := json.Marshal(data)
+	for i := 0; i < len(data); i++ {
+		byte, err := json.Marshal(data[i])
 
-	if err != nil {
-		fmt.Printf("Error: %s", err.Error())
-	}
-	f, err := os.OpenFile("data.json", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		log.Println(err)
-	}
-	defer f.Close()
-	if _, err = f.Write(byte); err != nil {
-		log.Println(err)
+		if err != nil {
+			fmt.Printf("Error: %s", err.Error())
+		}
+		f, err := os.OpenFile("data.json", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			log.Println(err)
+		}
+		defer f.Close()
+		if _, err = f.Write(byte); err != nil {
+			log.Println(err)
+		}
 	}
 }
 
@@ -133,21 +140,18 @@ func ScrapeTable() {
 							})
 						})
 					})
-
 					// Save the body and head inside a map
 					data_map[header[index]] = titlebody.Text()
+					// WriteToCSV(data_array, header)
 				})
 				//  Append the map in an array
 				data_array = append(data_array, data_map)
 			})
+
 		})
 
+		WriteToJSON(data_array)
 	})
-	// fmt.Println(header)
-
-	WriterToCSV(data_array, header)
-	// WriteToJSON(data_array)
-
 }
 
 func main() {
