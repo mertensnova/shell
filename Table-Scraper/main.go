d4837c1f6b7c9d2e51d38a71b8c71c5d4e824458
amr8644 <amr8644@gmail.com>
2023-02-26 18:57:11 +0300

    Refactoring

--- Table-Scraper/main.go
+++ Table-Scraper/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/csv"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,25 +11,20 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-type Data map[string]interface{}
-
-var myMapSlice []Data
-
-func WriteToCSV(data map[string]string, header []string) {
+func WriteToCSV(header []string) {
 	f, err := os.OpenFile("data.csv", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		log.Fatalf("failed creating file: %s", err)
 	}
-	data_array := []map[string]string{data}
-	unique_headers := RemoveDuplicates(header)
-	w := NewWriter(csv.NewWriter(f), unique_headers)
+
+	w := NewWriter(csv.NewWriter(f), header)
 
 	err = w.WriteHeader()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = w.WriteAll(data_array)
+	// err = w.Write(header)
 
 	if err != nil {
 		log.Fatalln(err)
@@ -39,31 +33,13 @@ func WriteToCSV(data map[string]string, header []string) {
 	f.Close()
 }
 
-func WriteToJSON(data map[string]string) {
+func main() {
+	start := time.Now()
+	var headers []string
 
-	byte, err := json.Marshal(data)
-
-	if err != nil {
-		fmt.Printf("Error: %s", err.Error())
-	}
-	f, err := os.OpenFile("data.json", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		log.Println(err)
-	}
-	defer f.Close()
-	if _, err = f.Write(byte); err != nil {
-		log.Println(err)
-	}
-
-}
-
-func ScrapeTable() {
-	data_map := make(map[string]string)
-	var header []string
-	// var bod []string
-
+	var rows []string
 	// Request the HTML page.
-	res, err := http.Get("https://www.the-numbers.com/box-office-records/worldwide/all-movies/cumulative/all-time")
+	res, err := http.Get("https://datatables.net/examples/styling/display.html")
 	if err != nil {
 		log.Println(err)
 	}
@@ -79,37 +55,30 @@ func ScrapeTable() {
 		log.Println(err)
 	}
 
-	doc.Find("table").Each(func(i int, table *goquery.Selection) {
-		// Get body of the table
-		table.Find("tbody").Each(func(i int, body *goquery.Selection) {
-			body.Find("tr").Each(func(i int, rowbody *goquery.Selection) {
-				rowbody.Find("td").Each(func(index int, titlebody *goquery.Selection) {
-					// Get head of the table
-					table.Find("thead").Each(func(i int, head *goquery.Selection) {
-						head.Find("tr").Each(func(i int, rowhead *goquery.Selection) {
-							rowhead.Find("th").Each(func(i int, titlehead *goquery.Selection) {
-								// Append the head to the array
-								header = append(header, titlehead.Text())
-							})
-						})
-					})
-					// Save the body and head inside a map
-
-					data_map[header[index]] = fmt.Sprintf(titlebody.Text())
-				})
-
-				WriteToJSON(data_map)
-				WriteToCSV(data_map, header)
-			})
-
+	doc.Find("table").Find("thead").Find("tr").Each(func(i int, s *goquery.Selection) {
+		s.Find("th").Each(func(i int, s *goquery.Selection) {
+			headers = append(headers, s.Text())
 		})
 	})
 
-}
+	doc.Find("table").Find("tbody").Find("tr").Each(func(i int, s *goquery.Selection) {
+		s.Find("td").Each(func(i int, s *goquery.Selection) {
+			rows = append(rows, s.Text())
+			// rows = append(rows, s.Text())
+		})
+	})
+	var data map[string][]string
 
-func main() {
-	start := time.Now()
-	ScrapeTable()
+	for _, header := range headers {
+		for _, row := range rows {
+			data[header] = append(data[header], row)
+			// data = append(data, map[string]string{
+			// 	header: row,
+			// })
+			fmt.Println(data)
+		}
+	}
+
 	elapsed := time.Since(start)
 	fmt.Printf("\n\nTime took %s", elapsed)
 }
