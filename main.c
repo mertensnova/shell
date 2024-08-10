4018db0005edc38123e8b924ede0030cc71e42f2
nyx6965 <72759681+nyx6965@users.noreply.github.com>
2024-08-10 21:34:53 +0300

    Trying to understand pipes

--- main.c
+++ main.c
@@ -2,46 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define MAX_PATH 1024
 void path_get(char *cmd);
-int redirect();
-
 
 int main(void) {
-  while (true) {
-    printf("\n$ ");
-    fflush(stdout);
+  printf("\n$ ");
 
-    char input[100];
-    char *s = fgets(input, 100, stdin);
+  char input[100];
+  char *s = fgets(input, 100, stdin);
 
-    char *args[] = {input, NULL};
-    input[strlen(input) - 1] = '\0';
+  input[strlen(input) - 1] = '\0';
 
-    path_get(input);
-  };
+  path_get(input);
   return 0;
 };
 
-
 void path_get(char *cmd) {
 
+  char buffer[1000];
   int fd[2];
+  int status;
+  int fd_pipe = pipe(fd);
 
-  pipe(fd);
+  pid_t cpid = fork();
+  if (cpid == -1) {
+    perror("fork()");
+  };
 
-  if (fork() == 0) {
-    dup2(fd[1], STDOUT_FILENO);
+  if (cpid == 0) {
+    //dup2(fd[1], STDOUT_FILENO);
     execlp(cmd, cmd, NULL);
-    printf("%s: command not found\n", cmd);
-  } else {
-    char buffer[10000];
-    ssize_t size = read(fd[0], buffer, 10000);
-    if ((size > 0) && (size < sizeof(buffer))) {
-      buffer[size] = '\0';
-      printf("%s\n", buffer);
-    }
-  }
-};
+   // printf("%s command not found", cmd);
+  }};
